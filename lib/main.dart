0af0cb4c4c3788595937b83f669f9985b8d017ba
AndroidDev-Rehan <rehanpsycho29@gmail.com>
2022-12-06 20:05:10 +0500

    milestone 8 2nd push.

--- lib/main.dart
+++ lib/main.dart
@@ -162,6 +162,7 @@ class AppState extends State<App> {
   Widget build(BuildContext context) {
 
     return GetMaterialApp(
+
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
